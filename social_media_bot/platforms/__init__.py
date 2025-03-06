8e71b4317308cd463650092eaeaedaa7c0c1cc8f
MR STRATEGIST <mr.strategistt@gmail.com>
2025-03-06 21:23:57 +0530

    Delete social_media_bot directory

--- social_media_bot/platforms/__init__.py
+++
@@ -1,7 +0,0 @@
-from .manager import PlatformManager
-from .base import SocialMediaPlatform
-from .devto import DevTo
-from .mastodon import Mastodon
-from .threads import Threads
-
-__all__ = ['PlatformManager', 'SocialMediaPlatform', 'DevTo', 'Mastodon', 'Threads'] 
